ea4acddc9ff1987a6145540ecfb342a3d4c112b9
e-manual-goldstein <shortalk@tcd.ie>
2020-04-12 17:15:30 -0400

    [Common] CommandService incomplete

---
+++ TTT.Common/Control/CommandService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TTT.Common.Control
+{
+    public class CommandService
+    {
+        Logger _logger;
+        Type[] _controllerTypes;
+        Dictionary<Type, MethodInfo> _actionDictionary = new Dictionary<Type, MethodInfo>();
+
+        public CommandService(Logger logger, Type[] controllerTypes)
+        {
+            _logger = logger;
+            //validate these
+            _controllerTypes = controllerTypes;
+        }
+
+        public string ExecuteCommand(GameCommand gameCommand)
+        {
+            if (!_actionDictionary.ContainsKey(gameCommand.CommandType))
+            {
+                if (!FindAction(gameCommand.CommandType))
+                    return "Unknown command";
+            }
+            return CreateControllerAndExecuteCommand(_actionDictionary[gameCommand.CommandType].DeclaringType);
+        }
+
+        private bool FindAction(Type argumentType)
+        {
+            _actionDictionary[argumentType] = _controllerTypes.SelectMany(conType => conType.GetType().GetMethods())
+                .SingleOrDefault(m => m.GetParameters().FirstOrDefault()?.ParameterType == argumentType);
+            return _actionDictionary[argumentType] != null;
+        }
+
+        private string CreateControllerAndExecuteCommand(Type controllerType)
+        {
+            //var controller = Activator.CreateInstance(controllerType, );
+            //_actionDictionary[gameCommand.CommandType].Invoke(, new object[] { gameCommand.SubCommand() });
+            throw new NotImplementedException();
+        }
+    }
+}
