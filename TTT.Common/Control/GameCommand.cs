0a793b9080bdc6966cb3e06c6d4aabe18b81818a
e-manual-goldstein <shortalk@tcd.ie>
2020-04-23 17:52:46 -0400

    [Common] Added error handling to GameCommand.TryParse(...)

--- TTT.Common/Control/GameCommand.cs
+++ TTT.Common/Control/GameCommand.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json.Serialization;
 
 namespace TTT.Common
 {
@@ -29,16 +30,22 @@ namespace TTT.Common
 
         public static bool TryParse(string message, out GameCommand gameCommand)
         {
-            try
-            {
-                gameCommand = JsonConvert.DeserializeObject<GameCommand>(message);
-                return gameCommand != null;
-            }
-            catch (Exception ex)
-            {
-                gameCommand = null;
-                return false;
-            }
+            gameCommand = JsonConvert.DeserializeObject<GameCommand>(message, 
+                new JsonSerializerSettings() 
+                { 
+                    Error = (object sender, ErrorEventArgs eventArgs) => 
+                    { 
+                        //Fail silently
+                        eventArgs.ErrorContext.Handled = true; 
+                    } 
+                });
+            return gameCommand != null;
+        }
+
+        private static void ErrorHandler(object sender, ErrorEventArgs eventArgs)
+        {
+            
+            //_logger.Warning($"{eventArgs.ErrorContext.Error.Message}");
         }
     }
 }
